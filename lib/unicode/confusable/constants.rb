f046aea443caa29631ae724e74d7e89ca87ed339
Jan Lelis <mail@janlelis.de>
2016-03-14 10:49:12 +0100

    Release 1.0.1

--- lib/unicode/confusable/constants.rb
+++ lib/unicode/confusable/constants.rb
@@ -1,6 +1,6 @@
 module Unicode
   module Confusable
-    VERSION = "1.0.0".freeze
+    VERSION = "1.0.1".freeze
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + '/../../../data/').freeze
     INDEX_FILENAME = (DATA_DIRECTORY + '/confusable.marshal.gz').freeze
   end
