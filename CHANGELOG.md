f9ef3cefb2fc33ac6ece6c704767d09308c478c6
Jan Lelis <mail@janlelis.de>
2016-06-21 18:05:36 +0200

    Support Unicode 9.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,14 @@
 ## CHANGELOG
 
+### 1.1.0
+
+* Support Unicode 9.0
+
 ### 1.0.1
 
 * Fix: Only load index if skeleton method is actually invoked
 
 ### 1.0.0
 
-* Inital release
+* Initial release
 
--- lib/unicode/confusable/constants.rb
+++ lib/unicode/confusable/constants.rb
@@ -1,6 +1,7 @@
 module Unicode
   module Confusable
-    VERSION = "1.0.1".freeze
+    VERSION = "1.1.0".freeze
+    UNICODE_VERSION = "9.0.0".freeze
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + '/../../../data/').freeze
     INDEX_FILENAME = (DATA_DIRECTORY + '/confusable.marshal.gz').freeze
   end
