a53666145ce62edbfc1b73f0763cd4063b588dac
Jan Lelis <mail@janlelis.de>
2019-05-08 09:14:54 +0200

    Unicode 12.1

--- lib/unicode/confusable/constants.rb
+++ lib/unicode/confusable/constants.rb
@@ -3,7 +3,7 @@
 module Unicode
   module Confusable
     VERSION = "1.5.0"
-    UNICODE_VERSION = "12.0.0"
+    UNICODE_VERSION = "12.1.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/confusable.marshal.gz").freeze
   end
