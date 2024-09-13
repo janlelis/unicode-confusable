d4566a0b9dfcd6fc892b92bbd07358d74f586e6e
Jan Lelis <hi@syntaxtransformation.com>
2024-09-13 11:17:33 +0200

    Update Unicode version number

--- lib/unicode/confusable/constants.rb
+++ lib/unicode/confusable/constants.rb
@@ -3,7 +3,7 @@
 module Unicode
   module Confusable
     VERSION = "1.10.0"
-    UNICODE_VERSION = "15.1.0"
+    UNICODE_VERSION = "16.0.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/confusable.marshal.gz").freeze
   end
